40a7078d2f45307f0df449856de85cd3e69e3a3a
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-06-09 21:08:14 -0500

    Adicionando algumas notações, gets e settes, falta testar e fazer reserve engenir

--- ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/model/Tag.java
+++ ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/model/Tag.java
@@ -1,12 +1,14 @@
 package ifac.si.com.ifac_si_api.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Tag implements Serializable{
@@ -18,4 +20,31 @@ public class Tag implements Serializable{
 
     @Column(nullable = false)
     private String nome;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Post> posts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
