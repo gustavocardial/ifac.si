a056ef5c0b00af41f9dea297f50628dd0eca4e60
beatriz-paiva <beatriz.paiva.rb@gmail.com>
2024-06-10 21:10:19 -0300

    initial

--- ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package ifac.si.com.ifac_si_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ifac.si.com.ifac_si_api.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-    
-}
