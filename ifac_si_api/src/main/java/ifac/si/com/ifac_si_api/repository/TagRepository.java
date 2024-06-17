66e546a9b51f6db95c0783446f7a8fb128cc811f
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-06-17 14:43:53 -0500

    Tag e usuario repository busca

--- ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/repository/TagRepository.java
+++ ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/repository/TagRepository.java
@@ -1,9 +1,14 @@
 package ifac.si.com.ifac_si_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import ifac.si.com.ifac_si_api.model.Tag;
 
 public interface TagRepository extends JpaRepository<Tag, Long>{
-    
+    @Query(
+        "SELECT t FROM Tag t WHERE t.nome LIKE %?1%"
+    ) List<Tag> busca(String termoBusca);
 }
