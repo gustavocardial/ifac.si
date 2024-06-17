ef5a5d1bebd186e57ea9d820bfdc6073aa8f5c5c
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-06-16 22:25:39 -0500

    TagController

---
+++ ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/controller/TagController.java
@@ -0,0 +1,68 @@
+package ifac.si.com.ifac_si_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ifac.si.com.ifac_si_api.model.Tag;
+import ifac.si.com.ifac_si_api.service.TagService;
+
+@RestController
+@RequestMapping("/tag")
+public class TagController implements IController<Tag>{
+
+    @Autowired
+    private TagService servico;
+
+    @Override
+    @GetMapping("/")
+    public ResponseEntity<List<Tag>> get() {
+        List<Tag> registros = servico.get();
+        return new ResponseEntity<>(registros, HttpStatus.OK);
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<Tag> get(@PathVariable("id") Long id) {
+        Tag registro = servico.get(id);
+        return new ResponseEntity<>(registro, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Tag>> get(String termoBusca) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    @PostMapping("/")
+    public ResponseEntity<Tag> insert(@RequestBody Tag objeto) {
+        Tag registro = servico.save(objeto);
+        return new ResponseEntity<>(registro, HttpStatus.CREATED);
+    }
+
+    @Override
+    @PutMapping("/")
+    public ResponseEntity<Tag> update(@RequestBody Tag objeto) {
+        Tag registro = servico.save(objeto);
+        return new ResponseEntity<>(registro, HttpStatus.OK);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        servico.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+}
