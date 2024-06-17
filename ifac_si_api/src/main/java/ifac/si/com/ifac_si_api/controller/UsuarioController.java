eab8e41ec0fcac991428e3e965cf12318b14f6ad
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-06-17 14:23:53 -0500

    UsuarioController

---
+++ ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/controller/UsuarioController.java
@@ -0,0 +1,62 @@
+package ifac.si.com.ifac_si_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import ifac.si.com.ifac_si_api.model.Usuario;
+import ifac.si.com.ifac_si_api.service.UsuarioService;
+
+public class UsuarioController implements IController<Usuario>{
+
+    @Autowired
+    private UsuarioService servico;
+
+    @Override
+    @GetMapping("/")
+    public ResponseEntity<List<Usuario>> get() {
+        List<Usuario> registros = servico.get();
+        return new ResponseEntity<>(registros, HttpStatus.OK);
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> get(Long id) {
+        Usuario registro = servico.get(id);
+        return new ResponseEntity<>(registro, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Usuario>> get(String termoBusca) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    @PostMapping("/")
+    public ResponseEntity<Usuario> insert(Usuario objeto) {
+        Usuario registro = servico.save(objeto);
+        return new ResponseEntity<>(registro, HttpStatus.CREATED);
+    }
+
+    @Override
+    @PutMapping
+    public ResponseEntity<Usuario> update(Usuario objeto) {
+        Usuario registro = servico.save(objeto);
+        return new ResponseEntity<>(registro, HttpStatus.OK);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(Long id) {
+        servico.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+}
