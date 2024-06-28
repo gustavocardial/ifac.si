94423254c17cda4c697e876bc79f29cb4ccf3525
beatriz-paiva <beatriz.paiva.rb@gmail.com>
2024-06-27 22:09:06 -0300

    .

--- ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package ifac.si.com.ifac_si_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ifac.si.com.ifac_si_api.model.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long>{
-    
-}
