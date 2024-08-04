053f1ddfab20c46cedb4aa0cb876e32b1cd75b94
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-08-03 19:53:39 -0500

    Arrumando dados em data.sql e enum em model.usuario

--- ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/model/Usuario.java
+++ ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/model/Usuario.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,6 +28,7 @@ public class Usuario implements Serializable{
     private String senha;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private ECargo cargo;
 
     @Column(nullable = false)
--- ifac_si_api/src/main/resources/data.sql
+++ ifac_si_api/src/main/resources/data.sql
@@ -1,21 +1,30 @@
 SET SESSION FOREIGN_KEY_CHECKS=0;
 
-INSERT INTO Usuario (nomeCompleto, nomeUsuario, senha, cargo, ativo)
+INSERT INTO Usuario (id, ativo, cargo, nome_completo, nome_usuario, senha)
 VALUES
-    ('João Silva', 'joao.silva', 'senha123', 'AUTOR', true),
-    ('Maria Santos', 'maria.santos', 'senha456', 'EDITOR', true),
-    ('Pedro Oliveira', 'pedro.oliveira', 'senha789', 'ADMIN', true);
+    (1, true, 'AUTOR', 'João Silva', 'joao.silva', 'senha123'),
+    (2, true, 'EDITOR', 'Maria Santos', 'maria.santos', 'senha456'),
+    (3, true, 'ADMIN', 'Pedro Oliveira', 'pedro.oliveira', 'senha789');
 
-INSERT INTO Tag (nome)
+INSERT INTO Tag (id, nome)
 VALUES
-    ('Tecnologia'),
-    ('Esportes'),
-    ('Viagem');
+    (1, 'Tecnologia'),
+    (2, 'Esportes'),
+    (3, 'Viagem');
 
-INSERT INTO Post (titulo, usuario_id, categoria_id, texto, data, legenda)
+INSERT INTO Post (id, data, legenda, texto, titulo, categoria_id, usuario_id)
 VALUES
-    ('Introdução à Programação', 1, 1, 'Este é um post sobre programação...', '2023-05-01', 'Programação'),
-    ('Dicas para Correr Melhor', 2, 2, 'Aqui estão algumas dicas para melhorar sua corrida...', '2023-05-02', 'Esportes'),
-    ('Destinos Incríveis para Viajar', 3, 3, 'Descubra os lugares mais incríveis para viajar...', '2023-05-03', 'Viagem');
+    (1, '2023-05-01', 'Introdução à Programação', 'Este é um post sobre programação...', 'Introdução à Programação', 1, 1),
+    (2, '2023-05-02', 'Dicas para Correr Melhor', 'Aqui estão algumas dicas para melhorar sua corrida...', 'Dicas para Correr Melhor', 2, 2),
+    (3, '2023-05-03', 'Destinos Incríveis para Viajar', 'Descubra os lugares mais incríveis para viajar...', 'Destinos Incríveis para Viajar', 3, 3);
+
+INSERT INTO post_tags (posts_id, tags_id)
+VALUES
+    (1, 1), -- Post "Introdução à Programação" com Tag "Tecnologia"
+    (2, 2), -- Post "Dicas para Correr Melhor" com Tag "Esportes"
+    (3, 3); -- Post "Destinos Incríveis para Viajar" com Tag "Viagem"
+
+INSERT INTO Categoria (id, nome) VALUES 
+(1, 'Projeto'), (2, 'Vaga'), (3, 'Pesquisa');
 
 SET SESSION FOREIGN_KEY_CHECKS=1;
