ee3e8a3252272ae6123766d9e3de4af868481cff
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-09-26 00:10:03 -0500

    Tentando adicionar icon para senha

--- ifac_si_app/src/app/component/login/login.component.ts
+++ ifac_si_app/src/app/component/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Usuario } from '../../model/usuario';
 
 @Component({
@@ -7,6 +7,9 @@ import { Usuario } from '../../model/usuario';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('enterButton') enterButton!: ElementRef;
+  @ViewChild('senha', { static: false }) senhaField!: ElementRef;
+  
   ngOnInit() {
     const inputs = document.querySelectorAll('input');
     inputs.forEach(input => {
@@ -17,9 +20,46 @@ export class LoginComponent {
           input.classList.remove('invalid');
         }
       });
+      input.addEventListener('input', () => {
+        this.updateButtonState();
+      });
     });
+
+    this.updateButtonState();
+
+    const togglePassword = document.querySelector('.toggle-password');
+    if (togglePassword) {
+      togglePassword.addEventListener('click', () => {
+        this.togglePasswordVisibility();
+      });
+    }
   }
-  
+
+  updateButtonState() {
+    const form = document.querySelector('form');
+    if (form && form.checkValidity()) {
+      this.enterButton.nativeElement.classList.add('valid');
+    } else if (form) {
+      this.enterButton.nativeElement.classList.remove('valid');
+    }
+  }
+
+  togglePasswordVisibility() {
+    const senhaField = this.senhaField.nativeElement;
+    const togglePassword = document.querySelector('.toggle-password');
+    if (senhaField && togglePassword) {
+      if (senhaField.type === 'password') {
+        senhaField.type = 'text';
+        togglePassword.classList.add('fa-eye-slash');
+        togglePassword.classList.remove('fa-eye');
+      } else {
+        senhaField.type = 'password';
+        togglePassword.classList.add('fa-eye');
+        togglePassword.classList.remove('fa-eye-slash');
+      }
+    }
+  }
+
   registro: Usuario = <Usuario>{};
   
   save() {
