159e29f7f6623034de8b77a817ef7677b5f7a339
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-09-30 19:08:35 -0500

    Implementando Tag

--- ifac_si_app/src/app/service/tag.service.ts
+++ ifac_si_app/src/app/service/tag.service.ts
@@ -15,15 +15,27 @@ export class TagService implements IService<Tag>{
   apiUrl: string = environment.API_URL + '/tag';
 
   get(termoBusca?: string): Observable<Tag[]> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl;
+    if (termoBusca) {
+      url += "busca/" + termoBusca;
+    }
+    return this.http.get<Tag[]>(url);
   }
   getById(id: number): Observable<Tag> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl + id;
+    return this.http.get<Tag>(url);
   }
   save(objeto: Tag): Observable<Tag> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl;
+    
+    if (objeto.id) {
+      return this.http.put<Tag>(url, objeto);
+    } else {
+      return this.http.post<Tag>(url, objeto);
+    }
   }
   delete(id: number): Observable<void> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl + id;
+    return this.http.delete<void>(url);
   }
 }
