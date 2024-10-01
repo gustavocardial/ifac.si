7c0e0abb042b725eaf4a35af867e3458639d6f13
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-09-30 19:08:18 -0500

    Implementando usuário

--- ifac_si_app/src/app/service/usuario.service.ts
+++ ifac_si_app/src/app/service/usuario.service.ts
@@ -15,15 +15,30 @@ export class UsuarioService implements IService<Usuario> {
   apiUrl: string = environment.API_URL + '/usuario';
   
   get(termoBusca?: string): Observable<Usuario[]> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl;
+    if (termoBusca) {
+      url += "busca/" + termoBusca;
+    }
+    return this.http.get<Usuario[]>(url);
   }
+
   getById(id: number): Observable<Usuario> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl + id;
+    return this.http.get<Usuario>(url);
   }
+
   save(objeto: Usuario): Observable<Usuario> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl;
+    
+    if (objeto.id) {
+      return this.http.put<Usuario>(url, objeto);
+    } else {
+      return this.http.post<Usuario>(url, objeto);
+    }
   }
+
   delete(id: number): Observable<void> {
-    throw new Error('Method not implemented.');
+    let url = this.apiUrl + id;
+    return this.http.delete<void>(url);
   }
 }
