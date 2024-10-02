a6c096ac5754ea30144fa2480cf5479a217c0df3
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-10-01 19:39:40 -0500

    Adicionando filtro cors e tentando fazer a ligação para os filtros

--- ifac_si_app/src/app/component/filters/filters.component.ts
+++ ifac_si_app/src/app/component/filters/filters.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../../service/categoria.service';
+import { TagService } from '../../service/tag.service';
+import { Categoria } from '../../model/categoria';
+import { Tag } from '../../model/tag';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit{
+  constructor (
+    private servicoCategoria : CategoriaService,
+    private servicoTag : TagService
+  ) {}
 
+  categorias: Categoria[] = Array<Categoria>();
+  tags: Tag[] = Array<Tag>();
+
+  ngOnInit(): void {
+    this.servicoCategoria.get().subscribe({
+      next: (resposta: Categoria[]) => {
+        this.categorias = resposta;
+      }
+    });
+
+    this.servicoTag.get().subscribe({
+      next: (resposta: Tag[]) => {
+        this.tags = resposta;
+      }
+    });
+  }
 }
