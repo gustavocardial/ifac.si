d11060994da056fbd059775828a5ecbaacda88bc
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-10-02 19:55:07 -0500

    AEeeeeee funcionou de alguma forma

--- ifac_si_app/src/app/component/posts-app/posts-app.component.ts
+++ ifac_si_app/src/app/component/posts-app/posts-app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../service/post.service';
+import { Post } from '../../model/post';
 
 @Component({
   selector: 'app-posts-app',
   templateUrl: './posts-app.component.html',
   styleUrl: './posts-app.component.css'
 })
-export class PostsAppComponent {
+export class PostsAppComponent implements OnInit{
+  posts: Post[] = Array<Post>();
 
+  constructor (
+    private postServico : PostService
+  ) {}
+
+  ngOnInit(): void {
+    this.postServico.get().subscribe({
+      next: (resposta: Post[]) => {
+        this.posts = resposta;
+      }
+    });
+  }
+
+  
 }
