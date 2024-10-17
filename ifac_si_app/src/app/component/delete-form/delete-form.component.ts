2c43badedbfb62935cf5b4e11015351bb99ba5b1
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-10-17 16:51:26 -0500

    Adicionando component deleteForm

---
+++ ifac_si_app/src/app/component/delete-form/delete-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-form',
+  templateUrl: './delete-form.component.html',
+  styleUrl: './delete-form.component.css'
+})
+export class DeleteFormComponent {
+
+}
