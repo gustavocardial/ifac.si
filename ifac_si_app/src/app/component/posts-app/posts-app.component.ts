39e1a352ad39f5c5739c8808d00263e31482f275
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-10-18 22:51:47 -0500

    AAAAAAAAAAAAAAA

--- ifac_si_app/src/app/component/posts-app/posts-app.component.ts
+++ ifac_si_app/src/app/component/posts-app/posts-app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { PostService } from '../../service/post.service';
 import { Post } from '../../model/post';
 
@@ -16,32 +16,41 @@ export class PostsAppComponent implements OnInit{
 
   constructor (
     private postServico : PostService,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private cdRef: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
     this.postServico.get().subscribe({
       next: (resposta: Post[]) => {
         this.posts = resposta;
+        setTimeout(() => this.setupButtonListeners(), 0);
       }
     });
   }
 
   ngAfterViewInit(): void {
+    this.setupButtonListeners();
+  }
+
+  private setupButtonListeners(): void {
+    this.listeners.forEach(listener => listener());
+    this.listeners = [];
+
+    // Adicione novos listeners
     this.deleteButtons.forEach(button => {
-      const listener = this.renderer.listen(button.nativeElement, 'click', (event) => {
+      const listener = this.renderer.listen(button.nativeElement, 'click', () => {
         alert('Delete selecionado');
       });
       this.listeners.push(listener);
     });
 
     this.editButtons.forEach(button => {
-      const listener = this.renderer.listen(button.nativeElement, 'click', (event) => {
+      const listener = this.renderer.listen(button.nativeElement, 'click', () => {
         alert('Edit selecionado');
       });
       this.listeners.push(listener);
     });
-
   }
 
   ngOnDestroy(): void {
