aa381c90536582a3ec3dd457ba46cff4dfd7e5e3
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-12-05 23:36:48 -0500

    Criada rota para notificação, agora começar a produzir

--- ifac_si_app/src/app/app-routing.module.ts
+++ ifac_si_app/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { PostsAppComponent } from './component/posts-app/posts-app.component';
 import { AddNewPostComponent } from './component/add-new-post/add-new-post.component';
+import { NotificationComponent } from './component/notification/notification.component';
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'view_posts', component: PostsAppComponent},
-  {path:'new_post', component: AddNewPostComponent}
+  {path:'new_post', component: AddNewPostComponent},
+  {path: 'notification', component: NotificationComponent}
 ];
 
 @NgModule({
