3e6833b43339a35ac91d2768285434c2c14881af
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2024-12-20 15:30:44 -0500

    Criando lista de teste

--- ifac_si_app/src/app/component/notification/notification.component.ts
+++ ifac_si_app/src/app/component/notification/notification.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './notification.component.css'
 })
 export class NotificationComponent {
-  
+  notifications = [
+    {
+      userName: 'joao.silva',
+      action: 'UPDATE',
+      title: 'Introdução à Programação'
+    },
+    {
+      userName: 'maria.santos',
+      action: 'DELETE',
+      title: 'Dicas para Correr Melhor'
+    },
+    {
+      userName: 'pedro.oliveira',
+      action: 'UPDATE',
+      title: 'Destinos Incríveis para Viajar'
+    },
+    {
+      userName: 'joao.silva',
+      action: 'DELETE',
+      title: 'Conceitos Básicos de Java'
+    },
+    {
+      userName: 'maria.santos',
+      action: 'UPDATE',
+      title: 'Maratona de São Paulo'
+    }
+  ];
 }
