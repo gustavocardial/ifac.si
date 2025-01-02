3d391ad65cea460fdb710c712a1e08f58a945f5f
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2025-01-02 14:17:15 -0500

    Criando a estruta html do adminView e fazendo uso do user service

--- ifac_si_app/src/app/admin-view/admin-view.component.ts
+++ ifac_si_app/src/app/admin-view/admin-view.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../service/usuario.service';
+import { Usuario } from '../model/usuario';
 
 @Component({
   selector: 'app-admin-view',
   templateUrl: './admin-view.component.html',
   styleUrl: './admin-view.component.css'
 })
-export class AdminViewComponent {
+export class AdminViewComponent implements OnInit{
+  usuarios: Usuario[] = Array<Usuario>();
+  constructor(private userServico: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.userServico.get().subscribe({
+      next: (resposta: Usuario[]) => {
+        this.usuarios = resposta;
+      }
+    })
+  }
+
 
 }
