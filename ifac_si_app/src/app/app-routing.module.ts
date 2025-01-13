4f9411b6a95532d7de42009044261a93acae96c6
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2025-01-13 15:59:37 -0500

    Separando rotas

--- ifac_si_app/src/app/app-routing.module.ts
+++ ifac_si_app/src/app/app-routing.module.ts
@@ -9,9 +9,15 @@ import { AdminViewComponent } from './component/admin-view/admin-view.component'
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'view_posts', component: PostsAppComponent},
-  {path:'new_post', component: AddNewPostComponent},
-  {path: 'notification', component: NotificationComponent},
-  {path: 'admin', component: AdminViewComponent}
+  {path: 'autor', children: [
+    {path:'new_post', component: AddNewPostComponent},
+  ]},
+  {path: 'editor', children: [
+    {path: 'notification', component: NotificationComponent},
+  ]},
+  {path: 'admin', children: [
+    {path: 'viewUsers', component: AdminViewComponent},
+  ]},
 ];
 
 @NgModule({
