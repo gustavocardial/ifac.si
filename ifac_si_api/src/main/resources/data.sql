960744ddd50c1f4ed5c715d3e2e6fd0e1b98a629
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2025-01-13 22:22:42 -0500

    Arrumando data.sql

--- ifac_si_api/src/main/resources/data.sql
+++ ifac_si_api/src/main/resources/data.sql
@@ -1,18 +1,18 @@
 SET SESSION FOREIGN_KEY_CHECKS=0;
 
-INSERT INTO Usuario (id, ativo, cargo, nome_completo, nome_usuario, senha)
+INSERT INTO usuarios (id, ativo, cargo, email, nome_usuario, senha)
 VALUES
-    (1, true, 'AUTOR', 'João Silva', 'joao.silva', 'senha123'),
-    (2, true, 'EDITOR', 'Maria Santos', 'maria.santos', 'senha456'),
-    (3, true, 'ADMIN', 'Pedro Oliveira', 'pedro.oliveira', 'senha789');
+    (1, true, 'AUTOR', 'JS@gmail.com', 'joao.silva', 'senha123'),
+    (2, true, 'EDITOR', 'MariaS@gmail.com', 'maria.santos', 'senha456'),
+    (3, true, 'ADMIN', 'Pedrao@gmail.com', 'pedro.oliveira', 'senha789');
 
-INSERT INTO Tag (id, nome)
+INSERT INTO tags (id, nome)
 VALUES
     (1, 'Tecnologia'),
     (2, 'Esportes'),
     (3, 'Viagem');
 
-INSERT INTO Post (id, data, legenda, texto, titulo, categoria_id, usuario_id)
+INSERT INTO posts (id, data, legenda, texto, titulo, categoria_id, usuario_id)
 VALUES
     (1, '2023-05-01', 'Introdução à Programação', 'Este é um post sobre programação...', 'Introdução à Programação', 1, 1),
     (2, '2023-05-02', 'Dicas para Correr Melhor', 'Aqui estão algumas dicas para melhorar sua corrida...', 'Dicas para Correr Melhor', 2, 2),
@@ -24,7 +24,7 @@ VALUES
     (2, 2), -- Post "Dicas para Correr Melhor" com Tag "Esportes"
     (3, 3); -- Post "Destinos Incríveis para Viajar" com Tag "Viagem"
 
-INSERT INTO Categoria (id, nome) VALUES
+INSERT INTO categorias (id, nome) VALUES
 (1, 'Projeto'), (2, 'Vaga'), (3, 'Pesquisa');
 
 SET SESSION FOREIGN_KEY_CHECKS=1;
