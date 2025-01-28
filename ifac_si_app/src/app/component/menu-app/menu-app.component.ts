58735236397bbed86e7a5285e22d1f38989806b2
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2025-01-27 23:49:43 -0500

    Começando separação de menu por usuário

--- ifac_si_app/src/app/component/menu-app/menu-app.component.ts
+++ ifac_si_app/src/app/component/menu-app/menu-app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from '../../model/menuItem';
 
 @Component({
   selector: 'app-menu-app',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './menu-app.component.css'
 })
 export class MenuAppComponent {
+  private menuItems: MenuItem[] = [
+    // Menu público (sem login)
+    {
+      caminho: '/view_posts',
+      label: 'Todas as publicações'
+    },
+    // Menu para usuários logados
+    {
+      caminho: '/administration/autor/new_post',
+      label: 'Criar nova publicação',
+      cargos: ['AUTOR', 'ADMIN']
+    },
+    {
+      caminho: '/meus-dados',
+      label: 'Meus dados',
+      cargos: ['USUARIO', 'AUTOR', 'ADMIN']
+    },
+    // Itens específicos para admin
+    {
+      caminho: '/admin/dashboard',
+      label: 'Dashboard',
+      cargos: ['ADMIN']
+    }
+  ];
 
 }
