80e08b6434bf62643fbbc4a56b6ea7b6a55d28aa
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2025-03-01 15:27:45 -0500

    Paginação funcionando aparentmente

--- ifac_si_app/src/app/model/page-request.ts
+++ ifac_si_app/src/app/model/page-request.ts
@@ -1,6 +1,6 @@
 export class PageRequest {
 
-    static readonly DEFAULT_PAGE_SIZE: number = 5;
+    static readonly DEFAULT_PAGE_SIZE: number = 6;
 
     page: number = 0;
     size: number = PageRequest.DEFAULT_PAGE_SIZE;
