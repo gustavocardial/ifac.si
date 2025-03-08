ee2e46928cdd5013ba97f91c5cbf53d9b2aad644
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2025-03-07 20:39:09 -0500

    Conseguindo arrumar a questão do icon de mostrar senha ou não no front

--- ifac_si_app/src/app/component/user-form/user-form.component.ts
+++ ifac_si_app/src/app/component/user-form/user-form.component.ts
@@ -24,7 +24,8 @@ export class UserFormComponent implements AfterViewInit{
   @ViewChild('confirmarSenha') confirmarSenhaInput!: ElementRef;
 
   usuario: Usuario = <Usuario>{};
-  showPassword = false;
+  showPasswordPrimary: boolean = false;
+  showPasswordConfirm: boolean = false;
 
   onSubmit(form: any): void {
 
@@ -51,19 +52,27 @@ export class UserFormComponent implements AfterViewInit{
     })
   }
 
-  togglePasswordVisibility() {
-    this.showPassword = !this.showPassword;
-    this.renderer.setAttribute(
-      this.senhaInput.nativeElement,
-      'type',
-      this.showPassword ? 'text' : 'password'
-    );
-    this.renderer.setAttribute(
-      this.confirmarSenhaInput.nativeElement,
-      'type',
-      this.showPassword ? 'text' : 'password'
-    );
+  togglePasswordVisibilityPrimary() {
+    this.showPasswordPrimary = !this.showPasswordPrimary;
   }
+  
+  togglePasswordVisibilityConfirm() {
+    this.showPasswordConfirm = !this.showPasswordConfirm;
+  }
+
+  // togglePasswordVisibility() {
+  //   this.showPassword = !this.showPassword;
+  //   this.renderer.setAttribute(
+  //     this.senhaInput.nativeElement,
+  //     'type',
+  //     this.showPassword ? 'text' : 'password'
+  //   );
+  //   this.renderer.setAttribute(
+  //     this.confirmarSenhaInput.nativeElement,
+  //     'type',
+  //     this.showPassword ? 'text' : 'password'
+  //   );
+  // }
 
   onCancel(): void {
     console.log('Cancelado!');
