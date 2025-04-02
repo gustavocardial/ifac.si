bd0f6a17cf7c3988b42381a5b75433084564509f
floraoliveirafranca@gmail.com <FloraOFF>
2025-04-02 17:11:46 -0500

    Criando comparation component

---
+++ ifac_si_app/src/app/component/comparation-view/comparation-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comparation-view',
+  templateUrl: './comparation-view.component.html',
+  styleUrl: './comparation-view.component.css'
+})
+export class ComparationViewComponent {
+
+}
