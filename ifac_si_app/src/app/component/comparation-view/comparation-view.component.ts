1504e6b52473a9d98a44896c46847ca94c51ea6e
floraoliveirafranca@gmail.com <FloraOFF>
2025-04-03 09:21:39 -0500

    Começando a criar comparation component

--- ifac_si_app/src/app/component/comparation-view/comparation-view.component.ts
+++ ifac_si_app/src/app/component/comparation-view/comparation-view.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-comparation-view',
   templateUrl: './comparation-view.component.html',
   styleUrl: './comparation-view.component.css'
 })
-export class ComparationViewComponent {
+export class ComparationViewComponent implements OnInit{
+  constructor ( 
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+  
+  ngOnInit(): void {
+    const id = this.route.snapshot.queryParamMap.get('postId');
+
+    if (id) {
+      alert(`Post editado com id: ${id}`)
+    }
+  }
 
 }
--- ifac_si_app/src/app/component/notification/notification.component.ts
+++ ifac_si_app/src/app/component/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notification',
@@ -6,35 +7,49 @@ import { Component } from '@angular/core';
   styleUrl: './notification.component.css'
 })
 export class NotificationComponent {
+
+  constructor (private router: Router) {}
+
   notifications = [
     {
+      id: 0,
       userName: 'joao.silva',
       action: 'UPDATE',
       title: 'Introdução à Programação'
     },
     {
+      id: 1,
       userName: 'maria.santos',
       action: 'DELETE',
       title: 'Dicas para Correr Melhor'
     },
     {
+      id: 2,
       userName: 'pedro.oliveira',
       action: 'UPDATE',
       title: 'Destinos Incríveis para Viajar'
     },
     {
+      id: 3,
       userName: 'joao.silva',
       action: 'POST',
       title: 'Conceitos Básicos de Java'
     },
     {
+      id: 4,
       userName: 'maria.santos',
       action: 'UPDATE',
       title: 'Maratona de São Paulo'
     }
   ];
 
-  alertTeste(): void {
-    alert('teste');
+  alertTeste(notification: any): void {
+    alert(`ID: ${notification.title}, Action: ${notification.action}`);
+
+    if(notification.action == 'UPDATE') {
+      this.router.navigate(['/administration/editor/comparation_post'], {
+        queryParams: {postId: notification.id}
+      })
+    }
   }
 }
