2fedf4e56cbf0d44c01771d7f164c5ed894e9ea5
FloraOFF <floraoliveirafranca@gmail.com>
2025-08-19 15:59:48 -0500

    refactor: Terminando a implementação de ProfissionalController.java

--- ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/controller/ProfissionalController.java
+++ ifac_si_api/src/main/java/ifac/si/com/ifac_si_api/controller/ProfissionalController.java
@@ -5,12 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import ifac.si.com.ifac_si_api.model.Profissional.Profissional;
+import ifac.si.com.ifac_si_api.model.Profissional.DTO.ProfissionalDTO;
 import ifac.si.com.ifac_si_api.service.ProfissionalService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -35,6 +41,34 @@ public class ProfissionalController {
     public ResponseEntity<Profissional> get(@PathVariable("id") Long id) {
         Profissional registro = servico.get(id);
         return new ResponseEntity<>(registro, HttpStatus.OK);
+    }
+
+    @GetMapping("/busca/{termoBusca}")
+    @Operation(summary = "Buscar ações por um termo de busca")
+    public ResponseEntity<List<Profissional>> get(String termoBusca) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @PostMapping("/")
+    @Operation(summary = "Inserir novo profissional")
+    public ResponseEntity<?> insert(ProfissionalDTO objeto, @RequestParam(value = "perfil", required = true) MultipartFile perfilFile) {
+        Profissional registro = servico.save(objeto, perfilFile);
+        return new ResponseEntity<>(registro, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/")
+    @Operation(summary = "Atualizar profissional existente")
+    public ResponseEntity<Profissional> update(Profissional objeto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar profissional pelo Id")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        servico.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
      
 }
